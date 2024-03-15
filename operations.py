476f8ff085c332d6e98bd9f0f17a73b5103a7ba3
Wojciech Dawid <wojciech.dawid@wat.edu.pl>
2024-03-15 13:06:50 +0100

    perform queries

--- operations.py
+++ operations.py
@@ -1,10 +1,19 @@
-from sqlalchemy import Engine, create_engine
+from sqlalchemy import Engine, create_engine, CursorResult
+from sqlalchemy.sql import text
 from geoalchemy2 import Geometry
+
+from config.api_config import DB_CONFIG
 
 
 class DbConnector:
 
-    @classmethod
-    def connect_db(db_name: str, echo: bool = False) -> Engine:
-        return create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}", echo=echo)
+    @staticmethod
+    def connect_db(db_name: str = DB_CONFIG['db_name']['weather'], echo: bool = False) -> Engine:
+        return create_engine(f"postgresql://{DB_CONFIG['username']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{db_name}", echo=echo)
 
+    @staticmethod
+    def query_db(query: str) -> CursorResult:
+        db_conn = DbConnector.connect_db()
+        with db_conn.connect() as conn:
+            statement = text(query)
+            return conn.execute(statement)
--- scrape_weather.py
+++ scrape_weather.py
@@ -1,8 +1,11 @@
 import requests
 import json
+from sqlalchemy.sql import text
+import pandas as pd
 
-from config.api_config import API_KEYS
-
+from config.api_config import API_KEYS, DB_CONFIG
+from operations import DbConnector
+"""
 API_key = API_KEYS[0]
 
 response_api = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API_key}')
@@ -10,6 +13,14 @@ data = response_api.text
 
 parse_json = json.loads(data)
 
-
-
 print(parse_json)
+"""
+table_name = DB_CONFIG["tables"]["10k"]
+query = f"Select count(*) from {table_name}"
+
+results = DbConnector.query_db(query)
+
+df = pd.DataFrame(results)
+print(df)
+
+
